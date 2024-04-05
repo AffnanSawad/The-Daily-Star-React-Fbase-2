8d4a05eda2bdd405a1afdd9060e3634161918ea0
Affnan Sawad <affnansawad2002@gmail.com>
2024-04-05 20:41:41 +0600

    setUp Done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLGGNvtpXHCDLll8UainwoGQ9tArMbBrc",
+  authDomain: "the-daily-star-react-fbase-2.firebaseapp.com",
+  projectId: "the-daily-star-react-fbase-2",
+  storageBucket: "the-daily-star-react-fbase-2.appspot.com",
+  messagingSenderId: "1068199929349",
+  appId: "1:1068199929349:web:4ba399d803c74f33c65e6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//export
+
+export default app ;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className=' bg-red-400 text-3xl' >this is home</h1>
+        </div>
+    );
+};
+
+export default Home;
